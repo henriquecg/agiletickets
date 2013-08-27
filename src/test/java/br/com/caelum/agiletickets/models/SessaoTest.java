a2d2e45123327aae35f5ac9b90c515872e65bd1d
Henrique Caixeta Gonçalves <henriquecg@gmail.com>
2013-08-27 11:50:47 -0300

    corrigindo bug

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -13,6 +13,14 @@ public class SessaoTest {
 
         Assert.assertTrue(sessao.podeReservar(1));
 	}
+	
+	@Test
+	public void deveVender2ingressoSeHa2vagas() throws Exception {
+		Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(2);
+
+        Assert.assertTrue(sessao.podeReservar(2));
+	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {

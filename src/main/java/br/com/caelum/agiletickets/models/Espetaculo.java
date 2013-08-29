4957290ce9a9412bbb6e96530bc72dfd410b60f1
claudio <ccnaraujo@yahoo.com.br>
2013-08-29 12:11:47 -0300

    implentacao strategy

--- src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
@@ -2,6 +2,7 @@ package br.com.caelum.agiletickets.models;
 
 import static com.google.common.collect.Lists.newArrayList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -12,8 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.hibernate.mapping.Array;
+import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
+import org.joda.time.Weeks;
 
 @Entity
 public class Espetaculo {
@@ -92,7 +96,10 @@ public class Espetaculo {
       */
 	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
 		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
-		return null;
+		
+					
+		return periodicidade.criaSessoes(this, inicio, fim, horario)	;
+		
 	}
 	
 	public boolean Vagas(int qtd, int min)
